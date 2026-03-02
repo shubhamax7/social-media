e98dc512af9187c140accafbb3b1b6b58e7ea545
Shubham Kumar <shubhamax7@gmail.com>
2026-03-02 09:51:14 +0530

    Implement Post delete functionality

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,21 +1,30 @@
+import { useContext } from "react";
+import { PostList } from "../store/post-list-store";
 import { MdDelete } from "react-icons/md";
 
 const Post = ({ post }) => {
+  const { deletePost } = useContext(PostList);
   return (
     <div className="card post-card" style={{ width: "18rem" }}>
       <div className="card-body">
         <h5 className="card-title">
           {post.title}
-          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          <span
+            className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+            onClick={() => deletePost(post.id)}
+          >
             <MdDelete />
-            <span class="visually-hidden">unread messages</span>
+            <span className="visually-hidden">unread messages</span>
           </span>
         </h5>
         <p className="card-text">{post.body}</p>
         {post.tags.map((tag) => (
-          <span className="badge text-bg-primary hashtag"> {tag}</span>
+          <span key={tag} className="badge text-bg-primary hashtag">
+            {" "}
+            {tag}
+          </span>
         ))}
-        <div class="alert alert-success reactions" role="alert">
+        <div className="alert alert-success reactions" role="alert">
           This post has been reacted by {post.reactions} people.
         </div>
       </div>
