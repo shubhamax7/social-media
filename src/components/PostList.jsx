3b099925ace164b151fcba1f21ecb07019cf1a8b
Shubham Kumar <shubhamax7@gmail.com>
2026-03-05 18:34:00 +0530

    Auto Fetchinging Default Data on Reload

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,13 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
+  const [dataFetched, setDataFetched] = useState(false);
 
-  const handleGetPostsClick = () => {
+  if (!dataFetched) {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
@@ -22,14 +23,13 @@ const PostList = () => {
         });
 
         addInitialPosts(normalizedPosts);
+        setDataFetched(true);
       });
-  };
+  }
 
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
-      )}
+      {postList.length === 0 && <WelcomeMessage />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
