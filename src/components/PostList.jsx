626703512a390da913d9512a6d7c0fc102262b41
Shubham Kumar <shubhamax7@gmail.com>
2026-03-05 18:16:43 +0530

    Fetch new Posts from server Functionality

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,35 @@
 import { useContext } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
-  const { postList } = useContext(PostListData);
-  console.log(postList);
+  const { postList, addInitialPosts } = useContext(PostListData);
+
+  const handleGetPostsClick = () => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        const normalizedPosts = data.posts.map((post) => {
+          let reactions = post.reactions;
+
+          if (typeof reactions === "object" && reactions !== null) {
+            const { likes = 0, dislikes = 0 } = reactions;
+            reactions = likes + dislikes;
+          }
+
+          return { ...post, reactions };
+        });
+
+        addInitialPosts(normalizedPosts);
+      });
+  };
+
   return (
     <>
+      {postList.length === 0 && (
+        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
+      )}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
